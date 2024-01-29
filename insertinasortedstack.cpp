d732a586558d1ad5b925f1f2c3ae3ecd0db9965c
BhargavaRam123 <abr191014358@gmail.com>
2024-01-29 21:24:35 +0530

    sorted a stack using recursion

--- insertinasortedstack.cpp
+++ insertinasortedstack.cpp
@@ -3,19 +3,14 @@
 using namespace std;
 void insertinasortedstack(stack<int> &s,int value)
 {
-   if(s.size()==0)
-  {
-    s.push(value);
-    return;
-  }
+
   int temp = s.top();
-  s.pop();
-  if(value>temp)
+  if(s.size()==0||value>temp)
   {
-    s.push(temp);
     s.push(value);
     return;
   }
+  s.pop();
   insertinasortedstack(s,value);
   s.push(temp);
 }
@@ -25,7 +20,7 @@ int main() {
   s.push(12);
   s.push(18);
   s.push(40);
-  insertinasortedstack(s,50);
+  insertinasortedstack(s,10);
   int len = s.size(); 
   cout<<"length is:"<<len<<endl;
   for(int i=0;i<len;i++)
