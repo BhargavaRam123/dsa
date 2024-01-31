cf71ca59c13a758fd472e07165f7beb0e2e18ab9
BhargavaRam123 <abr191014358@gmail.com>
2024-01-31 21:49:56 +0530

    created a queue without stl

---
+++ queuecreation.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+class Queue{
+  public:
+  int *arr;
+  int size;
+  int front;
+  int  rear;
+
+  Queue(int size){
+    arr = new int[size];
+    this->size = size;
+    front = -1;
+    rear = -1;
+  }
+
+  void push(int data)
+  {
+    if(rear==size-1)
+    {
+      cout<<"overflow condition"<<endl;
+      return;
+    }
+    if(front ==-1&&rear==-1)
+    {
+      front++;
+      rear++;
+      arr[front] = data;
+    }
+    else
+    {
+    rear++;
+    arr[rear] = data;
+    }
+  }
+
+  void pop(){
+     if(front==-1&&rear==-1)
+     {
+      cout<<"queue is empty cannot remove anything"<<endl;
+      return;
+     }
+     else if(front == rear)
+     {
+      arr[rear] = -1;
+      front = -1;
+      rear = -1;
+     }
+     else{
+      arr[front] = -1;
+      front++;
+     }
+  }
+
+  bool isempty()
+  {
+    if(front==rear&&front==-1)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  int getsize()
+  {
+    if(rear==front&&rear==-1)
+    return 0;
+    else
+    return rear-front +1;
+  }
+  int getfront()
+  {
+    if(rear==front&&rear==-1)
+    {
+      cout<<"stack is empty"<<endl;
+      return -1;
+    }
+    return arr[front];
+  }
+  void print()
+  {
+    cout<<"printing queue"<<endl;
+    for(int i=0;i<size;i++)
+    {
+      cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+  }
+};
+int main() {
+  cout << "Hello world!" << endl;
+  return 0;
+}
